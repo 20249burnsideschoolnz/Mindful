20cdb6bb7ba3656a1a24419c6b5aab6af7212e53
Xavier Cai <20249@burnside.school.nz>
2023-05-26 09:48:16 +1200

    Created login and account creation checks to prevent duplicate usernames

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, request, redirect
-from datetime import date
+from datetime import datetime
 import sqlite3
 app = Flask(__name__)
 
@@ -15,21 +15,27 @@ def home():
 
 @app.route('/my_posts', methods=['GET'])
 def my_posts():
-    conn = sqlite3.connect("mindful.db")
-    cur = conn.cursor()
-    sql = 'SELECT * FROM Gratitude_idea WHERE username = ?'
-    cur.execute(sql, (session['username'],))
-    results = cur.fetchall()
-    print(results)
-    return render_template("my_posts.html", results = results)
+    if 'username' in session:
+        print("logged in")
+        conn = sqlite3.connect("mindful.db")
+        cur = conn.cursor()
+        sql = 'SELECT * FROM Gratitude_idea WHERE username = ?'
+        cur.execute(sql, (session['username'],))
+        results = cur.fetchall()
+        return render_template("my_posts.html", results = results)
+    else:
+        print("not logged in")
+        return redirect("loginpage")
+
+     
 
 @app.post('/post_idea')
 def post_idea():
     conn = sqlite3.connect("mindful.db")
     cur = conn.cursor()
     sql = 'INSERT INTO Gratitude_idea (username, date, content) VALUES (?,?,?)'
-    cur.execute(sql, (session['username'], date.today(), request.form['post_idea'])) #Get username from cookie, get date from ?, get content from the form "create post" or "post_idea"
-    print(cur.execute(sql, (session['username'], date.today(), request.form['post_idea'])))
+    current_time = datetime.today().strftime('%Y-%m-%d')
+    cur.execute(sql, (session['username'], current_time, request.form['post_idea'])) #Get username from cookie, get date from ?, get content from the form "create post" or "post_idea"
     conn.commit()
     return redirect("my_posts")
 
@@ -42,21 +48,41 @@ def public_posts():
     return render_template("public_posts.html", results = results)
 
 #Route for the login page
-@app.route('/login', methods=['GET', 'POST'])    
-def login(): #If the given credentials are found in the user table then the user can login with those credentials. Loop through username column and password column until a legitimate combination has been found.
-    return render_template("login.html")
+@app.route('/loginpage')    
+def loginpage(): #If the given credentials are found in the user table then the user can login with those credentials. Loop through username column and password column until a legitimate combination has been found.   
+    return render_template("loginpage.html")
 
-@app.post('/create_account')
-def create_account():
-    session['username'] = request.form['username']
+@app.post('/login')
+def login():
     conn = sqlite3.connect("mindful.db")
     cur = conn.cursor()
-    sql = 'INSERT INTO User (username, password) VALUES (?,?)'
+    sql = 'SELECT username, password FROM User WHERE username = (?) AND password = (?)'
     cur.execute(sql, (request.form['username'], request.form['password']))
-    conn.commit()
-    session["username"] = request.form['username']
-    print(session["username"])
-    return redirect("/")
+    results = cur.fetchall()
+    print(results)
+    if not results:
+         print("doesnt exist")
+         msg = "Incorrect username or password"
+         return render_template("loginpage.html", msg = msg)
+    else:
+        return redirect('/')
+
+@app.post('/create_account') #First you need to check if a username already exists within the database.
+def create_account():
+    conn = sqlite3.connect("mindful.db")
+    cur = conn.cursor()
+    sql = 'SELECT username FROM User WHERE username = (?)' #Database will return a username if it exists
+    cur.execute(sql, (request.form['username'],))
+    username = cur.fetchone() 
+    if username == None: #If it doesnt exist then it will insert it into the database and the username session is filled.
+        sql = 'INSERT INTO User (username, password) VALUES (?,?)'
+        cur.execute(sql, (request.form['username'], request.form['password']))
+        conn.commit()
+        session["username"] = request.form['username']
+        return redirect("/")
+    else:
+        msg = "Sorry, username has been taken" #tells the user that the username is already taken 
+        return render_template("loginpage.html", msg = msg)
     
 
 if __name__ == "__main__":
